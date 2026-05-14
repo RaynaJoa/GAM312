5e1c57d6c49db9a1c999ca1c0a7702b6787066a5
RaynaJoa <rayna.joaquim@snhu.edu>
2026-05-14 15:22:38 -0400

    Initial Commit

---
+++ Source/GAM312/GAM312GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GAM312GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM312_API AGAM312GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
